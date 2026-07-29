ac3e55629b1b963f37be8ee61e8b4ed0b92e0676
wu21-web <wu2196674@icloud.com>
2026-07-29 22:01:35 +0900

    chore: recalc SHA

--- Formula/pixeval.rb
+++ Formula/pixeval.rb
@@ -6,22 +6,22 @@ class Pixeval < Formula
   on_macos do
     on_arm do
       url "https://github.com/Pixeval/Pixeval/releases/download/#{version}/Pixeval-#{version}-macos-arm64.zip"
-      sha256 "71f612385cdf0cabd8198304a930d6096c4c22d63d79cecf95ae101d8afd8133"
+      sha256 "049a68d9fc78ff49d85459f6d0d0c6fd1cf7485ae62bb9634d9e8fe05733e466"
     end
     on_intel do
       url "https://github.com/Pixeval/Pixeval/releases/download/#{version}/Pixeval-#{version}-macos-x64.zip"
-      sha256 "a88069e59ed3ab54352d8fc2df3fc5ff61e4539bf5787d8cc57e8e362b1c57bc"
+      sha256 "8691755f8a44124beee65179cc104fce1c2c086b2f78f4cbc503e187031536d0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Pixeval/Pixeval/releases/download/#{version}/Pixeval-#{version}-linux-arm64.tar.gz"
-      sha256 "dffdd58fb0a5a4efa3c677c394be6b97ad8e3f4b3c89b3e2087f3e84b2e93164"
+      sha256 "05bfee9193ac80d46cfc6f4a8c0cc329077395083a0d0f80b15cb3e4915df971"
     end
     on_intel do
       url "https://github.com/Pixeval/Pixeval/releases/download/#{version}/Pixeval-#{version}-linux-x64.tar.gz"
-      sha256 "276268d4093efc39121fcb083964f38f8ac3bec3cc1fda1adca3c2c6eb5452bf"
+      sha256 "34708701293b719dc2818360063296f4bffad5e777ff7079ce02002603f1fd1f"
     end
   end
 
