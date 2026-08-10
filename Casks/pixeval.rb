24966de3d09af29aa33b75cdfee11115baabaabd
Tao Xin <wu2196674@icloud.com>
2026-08-10 16:24:01 +0900

    bump cask: Pixeval to version 5.0.8

--- Casks/pixeval.rb
+++ Casks/pixeval.rb
@@ -1,13 +1,13 @@
 cask "pixeval" do
-  version "5.0.7"
+  version "5.0.8"
 
   on_arm do
-    sha256 "f2c8e948a3befdbaa729de0f4f282711dd995eab9ba4c2e5bd46780ca206e124"
+    sha256 "f40910d1bbb990cfd473c3f1b911b777379de0f367684a3c570ad59f2d26a877"
 
     url "https://github.com/Pixeval/Pixeval/releases/download/#{version}/Pixeval-#{version}-macos-arm64.zip"
   end
   on_intel do
-    sha256 "787ee8f8fb004540a41a8d7cd2dd4230bd928096b21cf8e74ef7f5be087467a5"
+    sha256 "29bfd95184acf9486e4c315e4afee95bd1015333df7ffda6de6298a097e243dc"
 
     url "https://github.com/Pixeval/Pixeval/releases/download/#{version}/Pixeval-#{version}-macos-x64.zip"
   end
